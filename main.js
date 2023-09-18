c4f84887510607a9fc164ff32a3d6204edd4b5ba
Gunish <gunishsharma20@gmail.com>
2023-09-17 20:51:12 -0600

    practising three

--- main.js
+++ main.js
@@ -139,6 +139,7 @@ renderer.setPixelRatio(window.devicePixelRatio)
 // make it a full screen canvas
 renderer.setSize(window.innerWidth, window.innerHeight)
 
+
 camera.position.setZ(30)
 
 //tell renderer to render the scene and the camera
@@ -149,11 +150,11 @@ renderer.render(scene, camera)
 //  (there are some built in ones)
 
 // heres a Torus
-const geometry = new THREE.TorusGeometry(10,3,16,100)
+const geometry = new THREE.TorusGeometry(10,3,6,100)
 // define a material to give it color and or texture (wrapping paper for geometry)
 //most materials rely on a light source to bounce off of them
 
-const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true} ) // this basic material does not require it
+const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true} ) // this basic material does not require it/
 //const material = new THREE.MeshStandardMaterial({color:0xFF6347 } ) // this standard material does
 // parameters for customizaton , like color, etc
 
@@ -162,8 +163,13 @@ const torus = new THREE.Mesh(geometry, material)
 
 //add object to screen
 scene.add(torus)
-// to see it gotta rerender(draw) the scene but you dont wanna do that its tideous so we finna set up a recurive function to do that
+// to see it gotta rerender(draw) the scene but you dont wanna do that its tideous so we finna set up a recursive function to do that
 //renderer.render(scene)
+
+const bufferGeo = new THREE.BoxGeometry(10,10,10)
+const bufferMat = new THREE.MeshBasicMaterial({color:'blue', wireframe:true})
+const buffer = new THREE.Mesh(bufferGeo,bufferMat)
+
 
 //need a lightsource to see that second material so lets add one: pointLight (there are more)
 const pointLight = new THREE.PointLight(0xffffff)
@@ -176,23 +182,30 @@ pointLight.position.set(20,20,20)
 scene.add(pointLight)
 scene.add(ambientLight)
 scene.add(lightHelper, gridHelper)
-
+//scene.add(buffer)
 const controls = new OrbitControls(camera, renderer.domElement)// instanate that class
+
+const randomNumbers = (min, max) => {
+	return Math.round(Math.random() * (max - min)) + min;
+}
 
 //Lets add multiple objects
 
 function addStar(){
   //create star object:
-  const starGeometry = new THREE.SphereGeometry(0.1, 24,24)
-  const starMaterial= new THREE.MeshStandardMaterial({color:0xffffff, wireframe:true})
+  const starGeometry = new THREE.SphereGeometry(0.24, 24,24)
+  const starMaterial= new THREE.MeshStandardMaterial({color:'red', wireframe:true})
   //combine
   const star = new THREE.Mesh(starGeometry,starMaterial)
   
   //get random x,y,z positions
-  const [x,y,z] = Array(2).fill().map(()=> THREE.MathUtils.randFloatSpread(100)) // creates an array which fills each element with random floats between -100 and 100
+  //const [x,y,z] = Array(2).fill().map(()=> THREE.MathUtils.randFloatSpread(100)) // creates an array which fills each element with random floats between -100 and 100
+  let x = randomNumbers(-25,25)
+  let y = randomNumbers(-25,25)
+  let z = randomNumbers(-25,25)
   star.position.set(x,y,z)
   //add to scene
- // scene.add(star)
+  scene.add(star)
 }
 
 Array(200).fill().forEach(addStar)
@@ -203,9 +216,9 @@ function animate(){
   renderer.render(scene, camera)
 
   torus.rotation.x += 0.01
-  torus.rotation.y += 0.005
+  torus.rotation.y += 0.05
   torus.rotation.z += 0.01
-
+  buffer.rotation.x += 0.05
   controls.update() // to show our mouse manipulations of the scene is captured
 }
 
