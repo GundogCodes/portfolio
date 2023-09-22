ec106e61dff8248f976470b12a2f87c45a6a0a14
Gunish <gunishsharma20@gmail.com>
2023-09-21 20:48:37 -0600

    added images and just projects css left

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
-require('./config/database.cjs');
+
 const app = require('./app-server.js');
 const PORT = process.env.PORT || 8005;
+
+
+app.listen(PORT, () => {
+    console.log(`API Listening on port ${PORT}`);
+  });
+  
