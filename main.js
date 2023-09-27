e902a87a155457021fa8dba50f98d56b2396130e
Gunish <gunishsharma20@gmail.com>
2023-09-27 00:03:12 -0600

    fixing css

--- main.js
+++ main.js
@@ -600,7 +600,7 @@ function animate(){
 opacity: 0;
 const observer = new IntersectionObserver((entries)=>{
   entries.forEach((entry)=>{
-    console.log(entry)
+    //console.log(entry)
     if (entry.isIntersecting){
       entry.target.classList.add('show')
     } else{
